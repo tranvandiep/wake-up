088a807b55e266e242ded0769ca8ca6d6361c08d
Planet-Ocean <theskysilence@gmail.com>
2017-12-18 23:13:49 +0700

    new updates to all

--- source/js/hung_vux_javascript_file.js
+++ source/js/hung_vux_javascript_file.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+
+
+    $(window).scroll(function(){
+                    if($(this).scrollTop() > 300 ) {
+                        $('#scroll-btn').fadeIn();
+                    } else {
+                                $('#scroll-btn').fadeOut();
+                        
+                            }
+                });
+                $('#scroll-btn').click(function(){ 
+                    $("html, body").animate({ scrollTop: 0 }, 500); 
+                     
+                });
+
+
+
+    
     //giam gia js
     $('#km-button2').click(function(){
         $('#km-button2').css("background-color" , "#34bfa3");
@@ -17,14 +35,19 @@ $(document).ready(function(){
     });
     //giam gia js
 
-
+    //------------------------------------------------------------------------------------------------------------------------//
 
     //marketing js
+
+     $('#mk-col2-div1').hide();
     $('#mk-head-btn1').click(function(){
         $('#mk-head-btn2').css("background-color" , "white");
         $('#mk-head-btn2  > a').css("color" , "#1DC7EA");
         $('#mk-head-btn1').css("background-color" , "#337ab7");
         $('#mk-head-btn1 > a').css("color" , "white");
+        $('#mk-col1-body').show();
+        $('#mk-col2-div1').hide();
+
    
     });
 
@@ -33,6 +56,8 @@ $(document).ready(function(){
         $('#mk-head-btn1  > a').css("color" , "#1DC7EA");
         $('#mk-head-btn2').css("background-color" , "#337ab7");
         $('#mk-head-btn2 > a').css("color" , "white");
+        $('#mk-col1-body').hide();
+        $('#mk-col2-div1').show();
   
     });
 
@@ -76,4 +101,13 @@ $(document).ready(function(){
 
     //marketing js
 
+
+    //------------------------------------------------------------------------------------------------------------------------//
+
+    //cai dat js
+        
+
+
+    //cai dat js
+     //------------------------------------------------------------------------------------------------------------------------//
 });

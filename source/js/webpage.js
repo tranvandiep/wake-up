119160558bc97b761fe7644676c3fc3d2804cdcd
Planet-Ocean <theskysilence@gmail.com>
2017-12-27 10:09:36 +0700

    Webpage menu is pushed

---
+++ source/js/webpage.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+   var count = 0;
+   $(window).scroll(function(e1){
+	    if($(this).scrollTop() > 200 ) {
+	        $('#main-navbar').css("background-color", "#474545");
+	    	} else {
+	                $('#main-navbar').css("background-color", "transparent");
+	        
+	            }
+	});
+
+
+
+
+});

722d2f605e869a40a3cbc2f19a51cd67bc1bcfa6
Black Crow <blackcrow.it@gmail.com>
2018-01-03 10:40:20 +0700

    Update Home Page 5

--- source/js/dang_ki_js.js
+++ source/js/dang_ki_js.js
@@ -1,1 +1,16 @@
 new WOW().init();
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 150 ) {
+            $('#main-navbar').css("background-color", "#474545");
+        }
+        else {
+            $('#main-navbar').css("background-color", "transparent");
+        }
+    });
+
+    $('#toggle-nav-button').click(function(){
+        $('#main-navbar').css("background-color", "#474545");
+    });
+});
